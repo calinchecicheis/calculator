941a8fb1cce8d80409bac03fd8fa4fbed600e455
Checicheis Calin <calinemail@gmail.com>
2019-05-27 16:26:18 +0300

    html done

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[number]')
+const operationButtons = document.querySelectorAll('[operation]')
+const equalsButton = document.querySelector('[equals]')
+const deleteButton = document.querySelector('[delete]')
+const clearButton = document.querySelector('[clear]')
+const previousOperandTextElement = document.querySelector('[previous]')
+const currentOperandTextElement = document.querySelector('[current]')
