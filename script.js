a63a8a3cffa5d34891c4806abee1e65948953326
Checicheis Calin <calinemail@gmail.com>
2019-05-28 15:24:59 +0300

    aproape gata

--- script.js
+++ script.js
@@ -1,7 +1,81 @@
-const numberButtons = document.querySelectorAll('[number]')
-const operationButtons = document.querySelectorAll('[operation]')
-const equalsButton = document.querySelector('[equals]')
-const deleteButton = document.querySelector('[delete]')
-const clearButton = document.querySelector('[clear]')
-const previousOperandTextElement = document.querySelector('[previous]')
-const currentOperandTextElement = document.querySelector('[current]')
+const numberButtons = document.querySelectorAll('.number')
+const operationButtons = document.querySelectorAll('.operation')
+const equalsButton = document.querySelector('.equals')
+const deleteButton = document.querySelector('.delete')
+const clearButton = document.querySelector('.clear')
+const negativeButton = document.querySelector('.negative')
+const historyDisplay = document.querySelector('.history')
+const currentDisplay = document.querySelector('.current-display')
+
+
+
+let current = ''
+let history = ''
+let operation = undefined
+let result = 0
+
+function clear() {
+    current = ''
+    history = ''
+    operation = undefined
+
+}
+
+function backspace() {
+    current = current.slice(0, -1)
+}
+
+function appendNumber(number) {
+    if (number === '.' && current.includes('.')) return
+    if (number ==='.') {current = '0'}
+    current = current.toString() + number.toString()
+}
+
+function selectOperation(operator) {
+    operation = operator
+    console.log(operation)
+    history = history + current + operation.toString()
+    current = ''
+    updateDisplay()
+}
+
+function calculate() {
+    history = history + current
+    current = eval(history)
+    history = ''
+    updateDisplay()
+    
+}
+
+function updateDisplay() {
+    currentDisplay.innerText = current
+    historyDisplay.innerText = history
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        appendNumber(button.innerText)
+        updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        selectOperation(button.innerText)
+    })
+})
+
+clearButton.addEventListener("click", () => {
+    clear()
+    updateDisplay()
+})
+
+deleteButton.addEventListener("click", () => {
+    backspace()
+    updateDisplay()
+})
+
+equalsButton.addEventListener("click", () => {
+    calculate()
+    
+})
